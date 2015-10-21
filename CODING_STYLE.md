c7aca4b961003379e7885ff345f4d8c0bfecd6b9
Peter MEUEL <peter@teriiehina.net>
2015-10-20 15:10:54 -1000

    Sorry, couldn't resist.
      - Put every braces on a newline
      - Change indentation to 2 spaces.

--- CODING_STYLE.md
+++ CODING_STYLE.md
@@ -1,2 +1,3 @@
 For indentation: 2 spaces, no tabs
 
+Every braces on a line by themselves
--- TVConference/Models.swift
+++ TVConference/Models.swift
@@ -13,31 +13,35 @@ import Foundation
 //------------------------------------------------------------------------------------------------
 
 // e.g. "UI", "UX", "DESIGN", "Mobile", "Marketing"
-struct Tags {
-    let name : String
+struct Tags
+{
+  let name : String
 }
 
 // NOTE: ça serait cool de passer cette struct en enum
 // e.g "fr", "en" ...
-struct Language {
-    let codeISO3166 : String
+struct Language
+{
+  let codeISO3166 : String
 }
 
 // Adresse du Moscone
-struct Address {
-    let name        : String
-    let address1    : String
-    let address2    : String?
-    let city        : String
-    let cityState   : String?
-    let postalCode  : Int
-    let country     : Language
+struct Address
+{
+  let name        : String
+  let address1    : String
+  let address2    : String?
+  let city        : String
+  let cityState   : String?
+  let postalCode  : Int
+  let country     : Language
 }
 
 // Advanced Debugging and the Address Sanitizer.ppt
-struct Presentation{
-    let url       : String
-    let timecode  : String?
+struct Presentation
+{
+  let url       : String
+  let timecode  : String?
 }
 
 // toto@mail.com
@@ -71,88 +75,101 @@ struct Mail
 }
 
 // Mike Swingler
-struct Presenter {
-    let name            : String
-    let mail            : Mail
-    let twitterLogin    : String?
-    let githubLogin     : String?
-    let imageSquareBig  : NSData?
+struct Presenter
+{
+  let name            : String
+  let mail            : Mail
+  let twitterLogin    : String?
+  let githubLogin     : String?
+  let imageSquareBig  : NSData?
 }
 
 // Advanced Debugging and the Address Sanitizer.ssa
-struct Subtitle {
-    let language  : Language
-    let url       : String
+struct Subtitle
+{
+  let language  : Language
+  let url       : String
 }
 
 // Advanced Debugging and the Address Sanitizer
-struct Video {
-    let conferenceEvent         : ConferenceEvent
-    let title                   : String
-    let tags                    : [Tags]
-    let presenters              : [Presenter]
-    let textDescription         : String
-    let subtitles               : [Subtitle]?
-    let spokenLanguages         : [Language]
-    let presentation            : Presentation?
-    let mailContentForAttendee  : String?
-    let imageSquareBig          : NSData?
+struct Video
+{
+  let conferenceEvent         : ConferenceEvent
+  let title                   : String
+  let tags                    : [Tags]
+  let presenters              : [Presenter]
+  let textDescription         : String
+  let subtitles               : [Subtitle]?
+  let spokenLanguages         : [Language]
+  let presentation            : Presentation?
+  let mailContentForAttendee  : String?
+  let imageSquareBig          : NSData?
 }
 
 // WWDC
-struct Conference {
-    let name              : String
-    let textDescription   : String
-    let imageTopBig       : NSData?
-    let imageSquareBig    : NSData?
-    let imageSquareSmall  : NSData?
-    let imageRectSmall    : NSData?
+struct Conference
+{
+  let name              : String
+  let textDescription   : String
+  let imageTopBig       : NSData?
+  let imageSquareBig    : NSData?
+  let imageSquareSmall  : NSData?
+  let imageRectSmall    : NSData?
 }
+
 
 // WWDC 2015
-struct ConferenceEvent    : PresentableInList {
-    let conference        : Conference
-    let tags              : [Tags]
-    let address           : Address
-    let dateStart         : NSDate
-    let dateStop          : NSDate?
-    let imageTopBig       : NSData?
-    let imageSquareBig    : NSData?
-    let imageSquareSmall  : NSData?
-    let imageRectSmall    : NSData?
+struct ConferenceEvent    : PresentableInList
+{
+  let conference        : Conference
+  let tags              : [Tags]
+  let address           : Address
+  let dateStart         : NSDate
+  let dateStop          : NSDate?
+  let imageTopBig       : NSData?
+  let imageSquareBig    : NSData?
+  let imageSquareSmall  : NSData?
+  let imageRectSmall    : NSData?
 }
 
-struct CarrousselElement : PresentableInList {
-    let videos  : [Video]
-    let image   : CarrousselImageType
-    let title   : String
+
+struct CarrousselElement : PresentableInList
+{
+  let videos  : [Video]
+  let image   : CarrousselImageType
+  let title   : String
 }
 
-struct Carroussel {
-    let title     : String
-    let type      : CarrousselType
-    let elements  : [CarrousselElement]
+
+struct Carroussel
+{
+  let title     : String
+  let type      : CarrousselType
+  let elements  : [CarrousselElement]
 }
 
 //------------------------------------------------------------------------------------------------
 // Enum
 //------------------------------------------------------------------------------------------------
 
-enum CarrousselType {
-    case Top
+enum CarrousselType
+{
+  case Top
 }
 
-enum CarrousselImageType {
-    case Big(url: String)
-    case Long(url: String)
-    case Squared(url: String)
-    case Small(url: String)
+enum CarrousselImageType
+{
+  case Big(url: String)
+  case Long(url: String)
+  case Squared(url: String)
+  case Small(url: String)
 }
 
 //------------------------------------------------------------------------------------------------
 // Protocol
 //------------------------------------------------------------------------------------------------
 
-protocol PresentableInList {
+protocol PresentableInList
+{
     
 }
