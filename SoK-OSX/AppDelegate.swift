ceb797ebfd448b4a905678b2a2caa7721452e84c
Peter MEUEL <peter@teriiehina.net>
2015-10-24 20:34:56 -1000

    Add an OSX target

---
+++ SoK-OSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SoK-OSX
+//
+//  Created by teriiehina on 24/10/2015.
+//  Copyright © 2015 Jeffrey Macko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ SoK-OSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SoK-OSX
+//
+//  Created by teriiehina on 24/10/2015.
+//  Copyright © 2015 Jeffrey Macko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
